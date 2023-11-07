a54a401ff1a75f2cfd37e3d1a6817dbc58173dc0
Kristi <kristinakaj42@gmail.com>
2023-11-07 15:34:09 -0500

    Build the transform function

--- etl_project_gdb.py
+++ etl_project_gdb.py
@@ -27,24 +27,36 @@ def extract(url, table_attribs):
     tables = data.find_all('tbody') # Extract all 'tbody' attributes of the HTML object 
     rows = tables[2].find_all('tr') # Extract all the rows of the index 2 table using the 'tr' attribute.
 
+    #check the contents of each row with attribute td, to make sure 1. the row is not empty, 2. the first column contains hyper link, 3. the third column doesn't contain '-'
     for row in rows:
         col = row.find_all('td')
         if len(col)!= 0:
             if col[0].find('a') is not None and '—' not in col[2]:
                 data_dict = {"Country": col[0].a.contents[0], 
-                             "GDP_USD_millions": col[2].contents[0]}
-                df1 = pd.DateFrame(data_dict, index =[0])
-                df = pd.concat([df,df1], ignore_index=True)
-
-
+                             "GDP_USD_millions": col[2].contents[0]} #store all entries matching the conditions to a dictionary with keys same as entries in table_attrib 
+                df1 = pd.DateFrame(data_dict, index =[0]) #create a new dataframe with the data in the dictionary
+                df = pd.concat([df,df1], ignore_index=True) #append all these dictionaries to the original dataframe
 
     return df
+
+
 
 def transform(df):
     ''' This function converts the GDP information from Currency
     format to float value, transforms the information of GDP from
     USD (Millions) to USD (Billions) rounding to 2 decimal places.
     The function returns the transformed dataframe.'''
+    
+    GDP_list = df['GDP_USD_millions'].tolist()  #convert the contents of a column into a list
+    GDP_list = [float("".join(x.split(',')))for x in GDP_list] #change all entries in the list into floats
+    GDP_list = [np.round(x/1000,2)for x in GDP_list] #divide by 1000 and round to 2 decimal places
+    df["GDP_USD_millions"] = GDP_list  #assign the modified list back to the dataframe column
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"}) #rename the column 
+    
+    return df
+
+
+
 
     return df
 
