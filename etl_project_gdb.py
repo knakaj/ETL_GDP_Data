76acb216d83992a949d6ec0d8c60f95440c72536
Kristi <kristinakaj42@gmail.com>
2023-11-07 15:49:47 -0500

    Added a Log Progress Function

--- etl_project_gdb.py
+++ etl_project_gdb.py
@@ -80,5 +80,9 @@ def run_query(query_statement, sql_connection):
 def log_progress(message):
     ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
     
-
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ',' + message + '\n') 
 
